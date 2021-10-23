8563cc26a9fe8b0c630e7a12173cb2617473606d
thebrijpatel <sadariyabrijesh@gmail.com>
2021-10-23 21:20:40 +0530

    Adding a navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import NavBar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+
 const App = () => {
-  return <div>APP component</div>;
+  return (
+    <>
+      <NavBar /> <Sidebar />
+      <div>APP component</div>
+    </>
+  );
 };
 
 export default App;
